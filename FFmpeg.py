526c3da7959ffb1787e0f511697d3734ca3d09d5
lostmypillow <47119111+lostmypillow@users.noreply.github.com>
2019-03-21 10:59:08 -0500

    Create FFmpeg.py

---
+++ FFmpeg.py
@@ -0,0 +1,4 @@
+#import subprocess 
+#os.chdir('C://Users/Alex/') 
+#subprocess.call(['ffmpeg', '-i', 'picture%d0.png', 'output.avi']) 
+#subprocess.call(['ffmpeg', '-i', 'output.avi', '-t', '5', 'out.gif'])
