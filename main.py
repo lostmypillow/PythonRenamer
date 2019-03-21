2a6469ec49bd23f40142a1dcc3b7e58217b23365
lostmypillow <47119111+lostmypillow@users.noreply.github.com>
2019-03-21 11:10:31 -0500

    Cleanup 2

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
-import os, sys, subsystem
+import os, sys
 def modify_name(pname):
     base = os.path.basename(pname)
     list = os.path.splitext(base)
     no_ext = list[0]
-    no_ext.replace(" ", "/")
-    no_ext.replace("(", " ")
-    print (no_ext)
+    split_ext = no_ext.split(".")
+    print(split_ext)
     
 def renaming(dict, entry):
     var = dict[entry]
     splitvar = var.split(".")
     split.append(splitvar)
 ask = input("Directory in which the files are in:")
+
+#for x in filenames:
+modify_name(ask)
+renaming(queue, 1)
+
+
+#test path: usr/local/b.i.n(128AAC).aac
 os.chdir(ask)
 
 split =[]
 ask = input("Directory in which the files are in:")
 os.chdir(ask)
 filenames = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-#for x in filenames:
-modify_name(ask)
-renaming(queue, 1)
-
-
-#test path: usr/local/bin(128AAC).aac
-
 # First go to the directory
 os.chdir(ask)
 
