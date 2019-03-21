218daccc4dac60e693247390ccc61bae49a43be7
lostmypillow <47119111+lostmypillow@users.noreply.github.com>
2019-03-21 10:57:02 -0500

    Added PowerShell method

--- main.py
+++ main.py
@@ -40,7 +40,16 @@ os.fchdir(fd)
 # Print current working directory
 print ("Current working dir : %s" % os.getcwd())
 
-
+input_func = None 
+try: input_func = raw_input 
+except NameError: input_func = input 
+  while True: parameters = input_func("enter symbol to be replaced:") 
+    p_cmd.replace("'x'", parameters) 
+    print(p_cmd) 
+    if parameters == "Stop": 
+        break 
+subprocess.call('C:\Windows\System32\powershell.exe', 'Get-ChildItem | Rename-Item -NewName {$_.name -replace ‘.’,’’ }', shell=True) subprocess.call('C:\Windows\System32\powershell.exe', 'Get-ChildItem | Rename-Item -NewName {$_.name -replace ‘mkv’,x }', shell=True) subprocess.call('C:\Windows\System32\powershell.exe', 'Get-ChildItem | Rename-Item -NewName {$_.name -replace ‘(’,’’ }', shell=True) 
+subprocess.call('C:\Windows\System32\powershell.exe', 'Get-ChildItem | Rename-Item -NewName {$_.name -replace ‘)’,’’ }', shell=True)
 
 #Renaming process
 #$ ls
