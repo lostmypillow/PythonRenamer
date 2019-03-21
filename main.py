1c8f067a4bef533b86b5c8bef5350c506908a5a6
lostmypillow <47119111+lostmypillow@users.noreply.github.com>
2019-03-21 10:43:09 -0500

    Added PowerShell method

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
-import os, sys
+import os, sys, subsystem
 def modify_name(pname):
     base = os.path.basename(pname)
     list = os.path.splitext(base)
-    queue[1] = list[0]
+    no_ext = list[0]
+    no_ext.replace("(", "_")
+    no_ext.replace(")"
     print(queue)
+    qnum += 1
+    print(qnum)
+    
 def renaming(dict, entry):
     var = dict[entry]
     splitvar = var.split(".")
     split.append(splitvar)
+ask = input("Directory in which the files are in:")
+os.chdir(ask)
+p_cmd = "Get-ChildItem | Rename-Item -NewName {$_.name -replace 'x', 'y'}"
+input_func = None
+try:
+    input_func = raw_input
+except NameError:
+    input_func = input
+while True:
+    parameters = input_func("enter symbol to be replaced:")
+    p_cmd.replace("'x'", parameters)
+    print(p_cmd)
+    if parameters == "Stop":
+        break
+subprocess.call('C:\Windows\System32\powershell.exe', 'Get-ChildItem | Rename-Item -NewName {$_.name -replace ‘.’,’’ }', shell=True)
+subprocess.call('C:\Windows\System32\powershell.exe', 'Get-ChildItem | Rename-Item -NewName {$_.name -replace ‘mkv’,x }', shell=True)
+subprocess.call('C:\Windows\System32\powershell.exe', 'Get-ChildItem | Rename-Item -NewName {$_.name -replace ‘(’,’’ }', shell=True)
+subprocess.call('C:\Windows\System32\powershell.exe', 'Get-ChildItem | Rename-Item -NewName {$_.name -replace ‘)’,’’ }', shell=True)
 queue = {}
+qnum = 1
 split =[]
-ask = input("hi:")
+ask = input("Directory in which the files are in:")
+os.chdir(ask)
+filenames = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+#for x in filenames:
 modify_name(ask)
 renaming(queue, 1)
 
 
-#test path: usr/local/bin.128AAC.aac
+#test path: usr/local/bin(128AAC).aac
 
 # First go to the directory
 os.chdir(ask)
