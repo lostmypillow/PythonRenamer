78c73a5cb3b1396e643f1f29208818f7b0abffff
lostmypillow <47119111+lostmypillow@users.noreply.github.com>
2019-03-25 10:18:58 -0500

    Better way of removing brackets

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import os, sys
-def modify_name(pname):
+def modify_name(pname, liist, w_dot):
     base = os.path.basename(pname)
-    list = os.path.splitext(base)
-    no_ext = list[0]
+    liist = os.path.splitext(base)
+    no_ext = liist[0]
     s1 = no_ext.split(".")
     s2 = "".join(s1)
-    s3 = s2.split("(")
-    print(s3)
-    if "128AAC)" in s3:
-        s3.remove("128AAC)")
-        print(s3)
-    join = "".join(s3)
-    print(join + list[1])
+    w_dot.append(s2)
+    w_dot.append(liist[1])
+def remove_text_inside_brackets(text, brackets="()[]"):
+    count = [0] * (len(brackets) // 2) # count open/close brackets
+    saved_chars = []
+    for character in text:
+        for i, b in enumerate(brackets):
+            if character == b: # found bracket
+                kind, is_close = divmod(i, 2)
+                count[kind] += (-1)**is_close # `+1`: open, `-1`: close
+                if count[kind] < 0: # unbalanced bracket
+                    count[kind] = 0  # keep it
+                else:  # found bracket to remove
+                    break
+        else: # character is not a [balanced] bracket
+            if not any(count): # outside brackets
+                saved_chars.append(character)
+    return ''.join(saved_chars)
+def remove2(x, lost):
+    print(x[1:-1] + lost[1])
+   
+
+list = []
+no_dot = []
 #ask for input in which the files are located
 ask = input("Directory in which the files are in:")
-modify_name(ask)
+modify_name(ask, list, no_dot)
+e =repr(remove_text_inside_brackets(no_dot[0]))
+remove2(e, no_dot)
+
 
 #test path: usr/local/b.i.n(128AAC).aac
 os.chdir(ask)
@@ -22,7 +42,6 @@ os.chdir(ask)
 split =[]
 
 os.chdir(ask)
-filenames = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 # First go to the directory
 os.chdir(ask)
 
